685dcad40364485209ca43bcf6439db004315668
Aditya <ranjanaditya452@gmail.com>
2025-06-15 09:12:28 +0530

    Display logic for each book in cards done

--- lib.js
+++ lib.js
@@ -1,7 +1,6 @@
 const myLibrary = [];
-let a=0;
 
-function Book(title,author,pages,readstat){
+function Book(id,title,author,pages,readstat){
 if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
@@ -17,6 +16,28 @@ if (!new.target) {
 
 
 function addBooks(title,author,pages,readstat)
-{ 
-  
+{ let id= crypto.randomUUID();
+  const newBook = new Book(id,title,author,pages,readstat);
+  myLibrary.push(newBook);
 }
+
+addBooks("hs","sd",2,"no");
+addBooks("hse","sd",2,"no");
+addBooks("hs2","sd",2,"no");
+
+let cardarea = document.querySelector(".cardArea");
+for(let i=0;i<myLibrary.length;i++)
+{
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.textContent=myLibrary[i].info();
+    cardarea.append(card);
+}
+
+
+function loopBook(){
+  for (let book of myLibrary){
+    console.log(book);
+  }
+}
+loopBook();
